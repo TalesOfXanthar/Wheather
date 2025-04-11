6678f14182c79765571230d66a1028ecef389d32
TalesOfXanthar <whitmantgarrity@gmail.com>
2025-04-10 17:36:52 -0700

    Sprites added
    New Sprites for the wheat and in progress changes to the placeholder crop script

--- scripts/placeholder_crop.gd
+++ scripts/placeholder_crop.gd
@@ -3,7 +3,7 @@ extends Node2D
 var frame = 0
 
 var plantAge = 0.0
-var plantGrowthIncrement = 1.0
+@export var plantGrowthIncrement = 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,8 +18,8 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed():
-	frame += 1
-	$AnimatedSprite2D.frame = frame
+	if frame == 3:
+		queue_free()
 
 
 func _on_timer_timeout():
