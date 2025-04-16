b4a51ca671d6ee4c27910b6842ae2cd005da2d59
The-Linker15 <146147008+The-Linker15@users.noreply.github.com>
2025-04-15 17:32:41 -0700

    Added moon assets and functionality

---
+++ scenes/moon.gd
@@ -0,0 +1,14 @@
+extends Node2D
+@export var time = 0
+
+func _process(delta: float) -> void:
+
+	
+	$Moon.frame = time
+
+
+func _on_timer_timeout() -> void:
+	if time >= 7:
+		time = 0
+	else:
+		time += 1
