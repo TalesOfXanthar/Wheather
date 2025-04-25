7b046e0711715bb5018c2be5a4d0dc1c616ab566
TalesOfXanthar <143358650+TalesOfXanthar@users.noreply.github.com>
2025-04-25 15:44:01 -0700

    Changed the Moon
    The moon now goes through a phase 4 times before mvoing onto the next and every single time a full moon night happens it has a 1/16 chance of being either a blood or blue moon (1/32 chance for each)

--- scripts/moon.gd
+++ scripts/moon.gd
@@ -1,18 +1,33 @@
 extends Node2D
-@export var time = 8
+@export var time = 0
 @export var isDay = true 
+var moonPhase = -0.5
+var specialMoonChance = 0
 func _ready():
 	pass
 
-func _process(delta: float) -> void:
+func _process(delta: float):
 	pass
 
-
-func _on_timer_timeout() -> void:
-	if isDay == true:
-		time = 8
-	elif time >= 7:
-		time = 0
+func _on_timer_timeout():
+	if isDay:
+		$Moon.animation = "Sun"
+		if time == 7:
+			isDay = false
+			moonPhase += 0.25
+			if moonPhase > 7:
+				moonPhase = -0.5
+			time = 0
+			specialMoonChance = randi_range(0, 31)
 	else:
-		time += 1
-	$Moon.frame = time
+		$Moon.animation = "MoonPhases"
+		if moonPhase >= 3.5 and moonPhase <= 4.25 and specialMoonChance < 2:
+			$Moon.animation = "SpecialMoons"
+			$Moon.frame = specialMoonChance
+		else:
+			$Moon.frame = round(moonPhase)
+		if time == 7:
+			isDay = true
+			time = 0
+	time += 1
+	
