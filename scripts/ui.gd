4ba04142cf1466eab8dd1d87a80cd8e88b657ff6
TalesOfXanthar <143358650+TalesOfXanthar@users.noreply.github.com>
2025-05-01 15:40:01 -0700

    Added Pepper Crop

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -15,3 +15,12 @@ func _on_carrot_button_gui_input(event: InputEvent) -> void:
 						GlobalTimeScript.cursorState = "Carrot"
 				MOUSE_BUTTON_RIGHT:
 					pass
+
+
+func _on_pepper_button_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed:
+			match event.button_index:
+				MOUSE_BUTTON_LEFT:
+						GlobalTimeScript.cursorState = "Pepper"
+				MOUSE_BUTTON_RIGHT:
+					pass
