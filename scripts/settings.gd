d233db189f2ae85b2bb07b920718fe847accd87e
The-Linker15 <146147008+The-Linker15@users.noreply.github.com>
2025-05-01 14:46:24 -0700

    Added and updated textures

---
+++ scripts/settings.gd
@@ -0,0 +1,9 @@
+extends MenuButton
+#func _ready():
+	#var quit = Button.new()
+	#quit.text = "Quit"
+	#quit.pressed.connect(_button_pressed)
+	#add_child(quit)
+#
+#func _button_pressed():
+	#get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
