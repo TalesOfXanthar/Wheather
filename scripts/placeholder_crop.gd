4484a0bf5379ba1afa3a02e7bc5e800551017409
TalesOfXanthar <143358650+TalesOfXanthar@users.noreply.github.com>
2025-05-05 15:05:03 -0700

    Removed useless code

--- scripts/placeholder_crop.gd
+++ scripts/placeholder_crop.gd
@@ -42,10 +42,5 @@ func return_to_ground():
 	frame = 0
 	print(GlobalTimeScript.playerMoney)
 
-
-func _on_static_body_2d_area_entered(area: Area2D) -> void:
-	print("entered")
-
-
 func _on_static_body_2d_body_entered(body: Node2D) -> void:
 	print("entered")
