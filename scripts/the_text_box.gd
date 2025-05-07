e7ecc49f191ac6ba68e64a071794da8df46b8e74
TalesOfXanthar <143358650+TalesOfXanthar@users.noreply.github.com>
2025-05-07 14:47:23 -0700

    Backrund changes and intro text

--- scripts/the_text_box.gd
+++ scripts/the_text_box.gd
@@ -1,6 +1,9 @@
 extends TextEdit
 var textUpdater
 
+func _ready() -> void:
+	GlobalTimeScript.textBox = "Welcome to Wheather, Version 0.1!"
+
 func _process(delta: float):
 	if GlobalTimeScript.textBox != textUpdater:
 		text = GlobalTimeScript.textBox
