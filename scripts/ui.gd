9f42f819cfe9e2d90dccffd7c61a4d07dfb729e0
The-Linker15 <146147008+The-Linker15@users.noreply.github.com>
2025-05-06 18:39:51 -0700

    Lots of stuff

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -8,8 +8,7 @@ func _on_wheat_button_gui_input(event: InputEvent) -> void:
 				MOUSE_BUTTON_RIGHT:
 					print ("Right")
 					$TabContainer/Shop.hide()
-					$"TabContainer/Upgrades/Wheat Upgrades".show()
-
+					
 func _on_carrot_button_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed:
 			match event.button_index:
@@ -38,5 +37,18 @@ func _on_corn_button_gui_input(event: InputEvent) -> void:
 
 
 func _on_price_pressed() -> void:
-	PlantDictionary["cropInfoDictionary"]["Wheat"]["Value"] = 70.00
+	PlantDictionary["cropInfoDictionary"]["Wheat"]["Value"] += 20.00
+	GlobalTimeScript.textBox = "Wheat has increaded in value"
 	$"TabContainer/Upgrades/Wheat Upgrades/Price".hide()
+
+func _on_button_pressed() -> void:
+	$TabContainer/Shop.show()
+	$TabContainer/Upgrades/Wheat/Economy.hide()
+	$TabContainer/Upgrades/Wheat/Resistance.hide()
+
+
+func _on_tornado_resistance_pressed() -> void:
+	PlantDictionary["cropInfoDictionary"]["Wheat"]["WeatherFrailty"]["Tornado"] += 20.00
+	GlobalTimeScript.textBox = "Wheat is now more resistant to tornados"
+	$"TabContainer/Upgrades/Wheat Upgrades/Tornado".hide()
+	$TabContainer/Upgrades/Exit.show()
