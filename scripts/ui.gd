8d5211932b8288ff0daa6979b7c5db3dc5b2e889
The-Linker15 <spcorona15@protonmail.com>
2025-05-09 21:28:18 -0700

    Wheat upgrades complete

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,16 +1,17 @@
 extends Control
 
+var sellPriceCost = 1000
+var seedPriceCost = 1000
+var tornadoCostPrice = 1000
+var rainCostPrice = 1000
+
 func _on_wheat_button_gui_input(event: InputEvent) -> void:
 		if event is InputEventMouseButton and event.pressed:
 			match event.button_index:
 				MOUSE_BUTTON_LEFT:
 						GlobalTimeScript.cursorState = "Wheat"
 				MOUSE_BUTTON_RIGHT:
-					print ("Right")
-					$TabContainer.hide()
-					$TabContainer/Upgrades/Wheat/Economy.show()
-					$TabContainer/Upgrades/Wheat/Resistance.show()
-					$TabContainer/Upgrades/Exit.show()
+					pass
 					
 func _on_carrot_button_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed:
@@ -20,7 +21,6 @@ func _on_carrot_button_gui_input(event: InputEvent) -> void:
 				MOUSE_BUTTON_RIGHT:
 					pass
 
-
 func _on_pepper_button_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed:
 			match event.button_index:
@@ -28,7 +28,6 @@ func _on_pepper_button_gui_input(event: InputEvent) -> void:
 						GlobalTimeScript.cursorState = "Pepper"
 				MOUSE_BUTTON_RIGHT:
 					pass
-
 
 func _on_corn_button_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed:
@@ -46,3 +45,27 @@ func _on_wheat_pressed() -> void:
 func _on_button_pressed() -> void:
 	$TabContainer/Upgrades/Selection.hide()
 	$TabContainer/Upgrades/Crops.show()
+
+func _on_sell_price_pressed() -> void:
+	if GlobalTimeScript.playerMoney >= sellPriceCost:
+		$"TabContainer/Upgrades/Selection/Wheat Upgrades/Economy/Sell Price".hide()
+		PlantDictionary.cropInfoDictionary["Wheat"]["Value"] *= 1.5
+	else:
+		GlobalTimeScript.textBox = "You can't buy that, you don't have enough money!"
+
+func _on_seed_price_pressed() -> void:
+	if GlobalTimeScript.playerMoney >= seedPriceCost:
+		$"TabContainer/Upgrades/Selection/Wheat Upgrades/Economy/Seed Price".hide()
+		PlantDictionary.cropInfoDictionary["Wheat"]["PlantCost"] *= 0.5
+	else:
+		GlobalTimeScript.textBox = "You can't buy that, you don't have enough money!"
+
+func _on_tornado_resistance_pressed() -> void:
+	if GlobalTimeScript.playerMoney >= tornadoCostPrice:
+		$"TabContainer/Upgrades/Selection/Wheat Upgrades/Resistance/Tornado Resistance".hide()
+		PlantDictionary.cropInfoDictionary["Wheat"]["WeatherFrailty"]["Tornado"] *= 1.5
+	else:
+		GlobalTimeScript.textBox = "You can't buy that, you don't have enough money!"
+
+func _on_rain_pressed() -> void:
+	pass # Replace with function body.
